a350005164dc29d5f322efef77f8696822cba15d
andan <andante1104@naver.com>
2024-03-09 11:08:13 +0900

    Remove : 불필요한 파일 제거 Chore : 프로젝트에 이용할 라이브러리 설치로 인한 package.json package-lock.json 파일 업데이트 Feat : App.tsx에  라우터 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-function App() {
-  return <></>;
-}
+import React from "react";
+import Router from "./router/Router";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Router />
+    </>
+  );
+};
 
 export default App;
---
+++ src/router/Router.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import ListPage from "../pages/ListPage";
+import SubmitFormPage from "../pages/SubmitFormPage";
+import DetailPage from "../pages/DetailPage";
+function Router() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* 
+            DetailPage : 글 상세페이지
+            HomePage : 메인 페이지
+            ListPage :  글 리스트 페이지
+            SubmitFormPage : 글 제출 폼 페이지  
+        */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/list" element={<ListPage />} />
+          <Route path="/form" element={<SubmitFormPage />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default Router;
